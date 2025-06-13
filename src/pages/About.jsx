8c5af51fbda9617fb8aae9c35448a2b3539c563c
shirocodes <ishirapt@gmail.com>
2025-06-13 05:13:06 +0300

    Feat:mobile responsiveness: Altered card display on different screens.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const About = () => {
   return (
-    <section className=' about-role container fade-in d-flex flex-column justify-content-center align-items-center min-vh-100 text-center'>
+    <section className=' about-role container fade-in d-flex flex-column mt-5 align-items-center min-vh-100 text-center text-md-start'>
       <h2 className='mb-3'>More than code — I deliver products & manage projects that succeed.</h2>
       <p className="lead mb-5">Fullstack Developer | Agile Project Manager | Problem Solver</p>
       
-      <div className='row mb-4 w-100'>
+      <div className='row row-cols-1 row-cols-sm-2 row-cols-lg-4 g-4'>
         <div className='col-6 col-md-3 mb-3'>
           <div className='card card-hover p-3 shadow-sm h-100'>
             <h6 className="mt-2">Clean, scalable code</h6>
